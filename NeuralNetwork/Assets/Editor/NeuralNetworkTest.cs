263d59a27875f2d051a6561526ab86b085ad36b2
Josh Morrow <josh@jcmorrow.com>
2017-01-15 10:51:20 -0800

    Training computes deltas

--- NeuralNetwork/Assets/Editor/NeuralNetworkTest.cs
+++ NeuralNetwork/Assets/Editor/NeuralNetworkTest.cs
@@ -3,22 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using NUnit.Framework;
+using Matrix = MathNet.Numerics.LinearAlgebra.Matrix<double>;
 
 class NeuralNetworkTest {
 
   [Test]
-  public void NeuralNetwork_Run_CanReturnSomething() {
+  public void NeuralNetwork_Run_ReturnsMatrixWithInputDimensions() {
     NeuralNetwork nn = new NeuralNetwork(new List<int> {2, 3, 2});
-    List<double> result = nn.Run(new List<double> { 1, 1 });
-    foreach(double i in result) {
-      Debug.Log(i);
+    const int inputRows = 4;
+    const int inputColumns = 2;
+    double[,] input = new double[inputRows, inputColumns] {
+      { 1, 1 },
+      { 0, 0 },
+      { 1, 0 },
+      { 0, 1 }
+    };
+
+    Matrix result = nn.Run(input);
+
+    Assert.AreEqual(inputRows, result.RowCount);
+    Assert.AreEqual(inputColumns, result.ColumnCount);
+    // Our weights should start out really small so all outputs should end up
+    // very close to .5 before any training has happened.
+    IEnumerator<double> results = result.Enumerate().GetEnumerator();
+    while(results.MoveNext()) {
+      Assert.True(results.Current > .4);
+      Assert.True(results.Current < .6);
     }
+  }
+
+  [Test]
+  public void NeuralNetwork_Train_RunsSmoothly() {
+    NeuralNetwork nn = new NeuralNetwork(new List<int> {2, 3, 2});
+    double[,] input = new double[,] {
+      { 1, 1 },
+      { 0, 0 },
+      { 1, 0 },
+      { 0, 1 }
+    };
+
+    double[,] target = new double[,] {
+      {1, 0},
+      {1, 0},
+      {0, 1},
+      {0, 1}
+    };
+
+    nn.Train(input, target);
   }
 
   [Test]
   public void NeuralNetwork_Sigmoid_ReturnsSigmoid() {
     NeuralNetwork nn = new NeuralNetwork(new List<int> {2, 3, 2});
 
-    // Debug.Log(nn.Sigmoid(-10));
+    double result = nn.Sigmoid(0);
+
+    Assert.AreEqual(.5, result);
   }
 }
--- NeuralNetwork/Assets/Scripts/NeuralNetwork.cs
+++ NeuralNetwork/Assets/Scripts/NeuralNetwork.cs
@@ -2,7 +2,7 @@ using System;
 using UnityEngine;
 using System.Collections.Generic;
 using Matrix = MathNet.Numerics.LinearAlgebra.Matrix<double>;
-using Vector = MathNet.Numerics.LinearAlgebra.Vector<double>;
+// using Vector = MathNet.Numerics.LinearAlgebra.Vector<double>;
 
 public class NeuralNetwork {
 
@@ -32,42 +32,63 @@ public class NeuralNetwork {
     }
   }
 
-  public List<double> Run(List<double> input) {
-    Vector inputVector = Vector.Build.DenseOfEnumerable(input);
-    int inputsCount = input.Count;
+  public Matrix Run(double[,] input) {
+    Matrix inputMatrix = Matrix.Build.DenseOfArray(input);
+    int numberOfCases = inputMatrix.RowCount;
 
     _layerInput = new List<Matrix>();
     _layerOutput = new List<Matrix>();
 
-    Matrix layerInput;
     for(int i = 0; i < layerCount; i++) {
+
+      Matrix layerInput;
+      Matrix layerOutput;
+
       if(i == 0) {
-        Matrix columnMatrix = inputVector.ToRowMatrix();
-        Matrix biasNode = Matrix.Build.Dense(1, 1, 1.0);
-        layerInput = weights[0] * columnMatrix.Append(biasNode).Transpose();
+        Matrix biasNodes = Matrix.Build.Dense(1, numberOfCases, 1.0);
+        layerInput = weights[i] * (inputMatrix.Transpose().Stack(biasNodes));
       } else {
-        Matrix columnMatrix = _layerOutput[_layerOutput.Count - 1].Transpose();
-        Matrix biasNode = Matrix.Build.Dense(1, 1, 1.0);
-        layerInput = weights[i] * columnMatrix.Append(biasNode).Transpose();
+        Matrix biasNodes = Matrix.Build.Dense(1, numberOfCases, 1.0);
+        Matrix previousLayerOutputs = _layerOutput[_layerOutput.Count - 1];
+        layerInput = weights[i] * previousLayerOutputs.Stack(biasNodes);
       }
 
-      Matrix layerOutput = Matrix.Build.Dense(
-        layerInput.RowCount,
-        layerInput.ColumnCount
-      );
-      for(int row = 0; row < layerInput.RowCount; row ++) {
-        for(int col = 0;col < layerInput.ColumnCount; col++) {
-          layerOutput[row, col] = Sigmoid(layerInput[row, col]);
-        }
-      }
+      layerOutput = layerInput.Map(val => Sigmoid(val));
 
       _layerInput.Add(layerInput);
       _layerOutput.Add(layerOutput);
     }
 
-    return new List<double>(
-      _layerOutput[_layerOutput.Count - 1].Enumerate()
-    );
+    return _layerOutput[_layerOutput.Count - 1].Transpose();
+  }
+
+  public void Train(double[,] input, double[,] target, double trainingRate = .2) {
+    List<Matrix> delta = new List<Matrix>();
+    Matrix inputMatrix = Matrix.Build.DenseOfArray(input);
+    Matrix targetMatrix = Matrix.Build.DenseOfArray(target);
+    int numberOfCases = inputMatrix.RowCount;
+
+    Run(input);
+
+    for(int index = layerCount - 1; index >= 0; index--) {
+      if(index == layerCount - 1) {
+        Matrix outputDelta = _layerOutput[index] - targetMatrix.Transpose();
+        // double error = (outputDelta.PointwisePower(2)).RowSums().Sum();
+        // I'm really not sure about this Transpose
+        Matrix sigmoid = _layerInput[index].Map(val => Sigmoid(val, true)).Transpose();
+        delta.Add(outputDelta * sigmoid);
+      } else {
+        Matrix lastDelta = delta[delta.Count - 1];
+        Matrix deltaPullback = weights[index + 1].Transpose() * lastDelta;
+        Matrix withoutBiases = deltaPullback.SubMatrix(
+          0, deltaPullback.RowCount - 1,
+          0, deltaPullback.ColumnCount
+        );
+        Matrix sigmoid = _layerInput[index].Map(val => Sigmoid(val, true));
+        // I'm really not sure about this Transpose
+        delta.Add(withoutBiases.Transpose() * sigmoid);
+      }
+    }
   }
 
   public double Sigmoid(double x, bool derivative = false) {
